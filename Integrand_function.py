766e7b93646e0f9f605b1efac1eb63e69c0d8269
Luigi Matera <luigi.matera@studio.unibo.it>
2022-12-06 11:14:27 +0100

    code updated

--- Integrand_function.py
+++ Integrand_function.py
@@ -9,16 +9,16 @@ beta = 5  # hopping energy
 
 """defining parameters of the integrand function"""
 num_points_xi = 40  # number of points of the integral function in the "overall" region
-energy_border_parameter = 1  # number of points added to the integral tails, measured in num_points_xi
+energy_border_parameter = 3  # number of points added to the integral tails, measured in num_points_xi
 
 """defining energy vector that will be used for plotting different curves"""
-energy = np.linspace(-2, 2, 5)
+energy = np.linspace(-5, 5, 11)
 
 """plotting the integrand function for the different values of energy"""
 fig, axs = plt.subplots(1)
 for E in energy:
     k, f = fD.integrand_function(E, energy_0, beta, num_points_xi, energy_border_parameter)
-    axs.plot(k, f, ".", label="Energy = {}".format(E))
+    axs.semilogy(k, f, ".", label="Energy = {}".format(E))
 axs.set_xlabel(r"$\xi$")
 axs.set_ylabel("Integrand function")
 plt.legend()
